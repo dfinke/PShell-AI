aab59d1296dea126aa06dae41f3408d8e394d98b
dfinke <finked@hotmail.com>
2024-10-15 19:13:56 -0400

    Update InstallModule.ps1 to dynamically set fullPath and refine Robocopy exclusions

--- InstallModule.ps1
+++ InstallModule.ps1
@@ -1,8 +1,11 @@
 param ($fullPath)
-
-$fullPath = Join-Path 'C:\Users\finke\OneDrive\Documentos\PowerShell\Modules' 'PShell-AI'
+#$fullPath = 'C:\Program Files\WindowsPowerShell\Modules\ImportExcel'
+if (-not $fullPath) {
+    $fullpath = $env:PSModulePath -split ":(?!\\)|;|," |
+        Where-Object {$_ -notlike ([System.Environment]::GetFolderPath("UserProfile")+"*") -and $_ -notlike "$pshome*"} |
+            Select-Object -First 1
+            $fullPath = Join-Path $fullPath -ChildPath "PShell-AI"
+}
 Push-location $PSScriptRoot
-
-Robocopy . $fullPath /mir /XD .vscode images .git .github CI __tests__ data mdHelp spikes /XF README.md README.original.md .gitattributes .gitignore filelist.txt install.ps1 InstallModule.ps1 PublishToGallery.ps1
-
+Robocopy . $fullPath /mir /XD .vscode images .git .github CI __tests__ data mdHelp spikes /XF README.md README.original.md appveyor.yml azure-pipelines.yml .gitattributes .gitignore filelist.txt install.ps1 InstallModule.ps1 PublishToGallery.ps1
 Pop-Location
